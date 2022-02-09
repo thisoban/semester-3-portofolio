a22e979aa33267c34d5adf00a4fcb9e2025e9cd1
thisoban <thisoban_1995@hotmail.com>
2022-02-09 16:47:51 +0100

    intial commit start up back end and datamodel added

---
+++ IP/CarBlog/backend/DataModel/Topics.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataModel
+{
+    class Topics
+    {
+        public int id { get; set; }
+        public string Topic { get; set; }
+    }
+}
